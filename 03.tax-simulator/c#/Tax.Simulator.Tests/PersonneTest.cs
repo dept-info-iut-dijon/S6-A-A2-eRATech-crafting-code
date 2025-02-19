32dbfedc16f3755b87a7a5385de10027e9f2a370
AxelBurillier <axel.burillier@iut-dijon.u-bourgogne.fr>
2025-02-19 15:09:58 +0100

    test : Ajout des tests unitaires pour les classes d'entités

---
+++ 03.tax-simulator/c#/Tax.Simulator.Tests/PersonneTest.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tax.Simulator.Entities;
+using Xunit;
+
+namespace Tax.Simulator.Tests
+{
+    public class PersonneTest
+    {
+        [Fact]
+        public void PersonneConstructorTest()
+        {
+            // Arrange & Act
+            Personne personne = new Personne(SituationsFamiliales.CELIBATAIRE, 10, 10, 1);
+
+            // Assert
+            personne.SituationFamiliale
+                .Should()
+                .Be(SituationsFamiliales.CELIBATAIRE);
+
+            personne.SalaireMensuel
+                .Should()
+                .Be(10);
+
+            personne.SalaireConjoint
+                .Should()
+                .Be(10);
+
+            personne.NbEnfants
+                .Should()
+                .Be(1);
+        }
+
+        [Fact]
+        public void SituationToStringTest()
+        {
+            SituationsFamilialesExtensions.StringToSituation("Marié/Pacsé")
+                .Should()
+                .Be(SituationsFamiliales.MARIE_PACSE);
+
+            SituationsFamilialesExtensions.StringToSituation("Célibataire")
+                .Should()
+                .Be(SituationsFamiliales.CELIBATAIRE);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => SituationsFamilialesExtensions.StringToSituation("Test"));
+        }
+    }
+}
