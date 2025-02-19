615a69f918bc98ce27742a6fdf41f99a6fd9ffc6
AxelBurillier <axel.burillier@iut-dijon.u-bourgogne.fr>
2025-02-19 09:10:28 +0100

    refactor : Ajout de l'énumération SituationsFamiliales On remplace les situations en chaine en énumération

---
+++ 03.tax-simulator/c#/Tax.Simulator.Entities/SituationsFamiliales.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tax.Simulator.Entities
+{
+    /// <summary>
+    /// Enumération pour les situations familiales
+    /// </summary>
+    public enum SituationsFamiliales
+    {
+        MARIE_PACSE = 2,
+        CELIBATAIRE = 1
+    }
+}
